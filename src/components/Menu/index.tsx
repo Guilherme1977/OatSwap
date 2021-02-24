932f057430e310c760e4dcd9ce88ac80f2ab80a6
oatswap-dev <contact@oatswap.org>
2021-02-24 13:59:13 +0800

    fix: Show the oat price

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,7 +4,7 @@ import { useWeb3React } from '@web3-react/core'
 import { allLanguages } from 'constants/localisation/languageCodes'
 import { LanguageContext } from 'hooks/LanguageContext'
 import useTheme from 'hooks/useTheme'
-// import useGetPriceData from 'hooks/useGetPriceData'
+import useGetPriceData from 'hooks/useGetPriceData'
 // import useGetLocalProfile from 'hooks/useGetLocalProfile'
 import { injected, bsc, walletconnect } from 'connectors'
 import links from './config'
@@ -13,8 +13,8 @@ const Menu: React.FC = (props) => {
   const { account, activate, deactivate } = useWeb3React()
   const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
   const { isDark, toggleTheme } = useTheme()
-  // const priceData = useGetPriceData()
-  // const cakePriceUsd = priceData ? Number(priceData.prices.Cake) : undefined
+  const priceData = useGetPriceData()
+  const cakePriceUsd = priceData ? Number(priceData.prices.OAT) : undefined
   // const profile = useGetLocalProfile()
 
   return (
@@ -38,7 +38,7 @@ const Menu: React.FC = (props) => {
       currentLang={selectedLanguage?.code || ''}
       langs={allLanguages}
       setLang={setSelectedLanguage}
-      cakePriceUsd={0}
+      cakePriceUsd={cakePriceUsd}
       // profile={profile}
       {...props}
     />
